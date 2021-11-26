b66343bc8dfcbf5e1b135444fdfcdd78d1022845
Mark Steadman <47225088+Steady5063@users.noreply.github.com>
2021-11-26 15:09:47 -0600

    Chore: update test case to include all appium content

--- _tests_/abctech.spec.js
+++ _tests_/abctech.spec.js
@@ -2,12 +2,25 @@ const { assert } = require("chai");
 const selenium = require('selenium-webdriver');
 const AxeWebdriverJS = require('@axe-core/webdriverjs');
 
-
-describe('Abcd Tech Spec', () => {
+//Test spec for the original space jam 1996 website.
+describe('Space Jam 1996', () => {
   let driver;
 
+  //This can be set to any desired capabiltie including Android/Chrome etc.
+  let desiredCaps = {
+    'automationName': 'xcuitest',
+    'platformName' : 'iOS',
+    'platformVersion' : '12.4',
+    'deviceName' : 'iPhone 12 Pro Max', 
+    'udid' : '78AE3521-165F-46F8-B08A-9F42546DFE57',
+    'browserName' : 'Safari'
+  };
+
+  let localServer = "http://localhost:4723/wd/hub"
+
  before(async () => {
-  driver = await new selenium.Builder().withCapabilities(selenium.Capabilities.chrome()).build();
+  driver = await new selenium.Builder().usingServer("http://localhost:4723/wd/hub")
+            .withCapabilities(desiredCaps).build();
   axeDriver = await new AxeWebdriverJS(driver);
 });
 
@@ -15,10 +28,11 @@ after(async () => {
   await driver.quit();
 });
  
+//Test case that checks the accessibility of the site
 it('is accessible', async () => {
-  await driver.get('http://abcdcomputech.dequecloud.com');
-  const results = await axeDriver.analyze();
-  assert(results.violations.length === 0, "More than zero a11y violations");
+  await driver.get('https://www.spacejam.com/1996/');
+  const axeResults = await axeDriver.analyze();
+  assert(axeResults.violations.length === 0, "More than zero a11y violations");
 });
 
 
